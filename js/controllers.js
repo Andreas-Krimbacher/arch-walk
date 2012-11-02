416d210f27ed7331d7ccd7cea83e6d3810f5e767
Andreas Krimbacher <andreas.krimbacher@gmail.com>
2012-11-02 02:07:59 +0100

    map and add marker

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,20 @@
 /* Controllers */
 
 archWalk.MapController = function( $scope ) {
+    console.log('MapController')
+    $scope.newMarker = null;
+
+    $scope.$on('addMarker', function(e,point){
+        $scope.newMarker = point;
+    });
+
+};
+
+archWalk.FormController = function( $scope,$rootScope ) {
+    console.log('FormController')
+    $scope.showMarker = function(point){
+        $rootScope.$broadcast('addMarker',  angular.copy(point));
+
+    }
 
 };
--- js/directives.js
+++ js/directives.js
@@ -7,21 +7,25 @@ archWalk.directive('map', function(Leaflet) {
         restrict: 'E',
         replace: true,
         template: '<div></div>',
+        require: archWalk.MapController,
         link: function(scope, element, attrs) {
+
             var map = Leaflet.map(attrs.id, {
-                center: [40, -86],
-                zoom: 10
+                center: [0,0],
+                zoom: 1
             });
             //create a CloudMade tile layer and add it to the map
             Leaflet.tileLayer('http://{s}.tile.cloudmade.com/123f560e5bab4afab1b85d73db1f813b/997/256/{z}/{x}/{y}.png', {
                 maxZoom: 18
             }).addTo(map);
 
-            //add markers dynamically
-            var points = [{lat: 40, lng: -86},{lat: 40.1, lng: -86.2}];
-            for (var p in points) {
-                Leaflet.marker([points[p].lat, points[p].lng]).addTo(map);
-            }
+
+
+            scope.$watch('newMarker', function(newPoint, oldPoint){
+                if(newPoint != null) {
+                    Leaflet.marker([newPoint.lat, newPoint.lng]).addTo(map);
+                }
+            },true);
         }
     };
 });
